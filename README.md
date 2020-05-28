22c71f08cb1f13bcab8c85c03e433db315555cc3
maxwieringa <maxwieringa01@gmail.com>
2020-05-28 11:53:43 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# portfolio-frontend-opdracht2-1920
+ Opdracht 2 frontend voor designers
---
+++ script.js
@@ -0,0 +1,40 @@
+var i = 0; //start punt van slideshow
+var images = [];
+
+//array met images voor boscross boekje
+images[0] = 'PortfolioItems/BoscrossBoekje.png';
+images[1] = 'PortfolioItems/BoscrossBoekje2.png';
+images[2] = 'PortfolioItems/BoscrossBoekje3.png';
+images[3] = 'PortfolioItems/BoscrossBoekje4.png';
+images[4] = 'PortfolioItems/BoscrossBoekje5.png';
+images[5] = 'PortfolioItems/BoscrossBoekje6.png';
+images[6] = 'PortfolioItems/BoscrossBoekje7.png';
+images[7] = 'PortfolioItems/BoscrossBoekje8.png';
+images[8] = 'PortfolioItems/BoscrossBoekje9.png';
+images[9] = 'PortfolioItems/BoscrossBoekje10.png';
+images[10] = 'PortfolioItems/BoscrossBoekje11.png';
+images[11] = 'PortfolioItems/BoscrossBoekje12.png';
+images[12] = 'PortfolioItems/BoscrossBoekje13.png';
+images[13] = 'PortfolioItems/BoscrossBoekje14.png';
+images[14] = 'PortfolioItems/BoscrossBoekje15.png';
+
+function changeImgPrev() {
+  i--;
+  if (i < 0) {
+    i = images.length - 1;
+  }
+  document.getElementById('slide').src = images[i];
+  console.log(i);
+}
+
+function changeImgNext() {
+  i++
+  if (i > images.length - 1) {
+    i = 0;
+  }
+  document.getElementById('slide').src = images[i];
+  console.log(i);
+}
+
+document.getElementById('prev').addEventListener("click", changeImgPrev);
+document.getElementById('next').addEventListener("click", changeImgNext);
